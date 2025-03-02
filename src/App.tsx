6272eaf0aa55e9c686c1c39939f2c6bd5b1088e4
Giho <90423010+Levytein@users.noreply.github.com>
2025-03-01 19:10:13 -0500

    Fixed routing issue

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import LevelOne from './views/LevelOne'
 function App() {
   return (
     
-   <BrowserRouter basename="/WhereisthatPokemon">
+   <BrowserRouter basename="/WhereisthatPokemon/">
         <Header />
 
       
